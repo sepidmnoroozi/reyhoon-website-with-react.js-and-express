4fc684184cd9131ba9d40f195fab59205fe41b10
mnoroozi <3pid.mollanoroozi@gmail.com>
2019-07-01 01:31:18 +0430

    routers updated and averageRate calculation added

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -9,36 +9,64 @@ restaurantRouter
     req.restaurant = {};
     next();
   })
+  //ok!
   .get("/restaurants/area/select", (req, res) => {
-    var area_query = req.query["‫‪area"];
-    // var categories_query = req.query["category"];
+    var area_query = req.query["area"];
     console.log(area_query);
-    restaurant.model.find({ area : area_query }, (error, foods) => {
+    var result = [];
+    restaurant.model.find({}, (error, foods) => {
+      if (error) {
+        res.send(error);
+      }
+      foods.forEach(element => {
+        var add = element["address"];
+        if (add["area"] == area_query){
+          result.push(element);
+        }      
+      });
+      res.send(result);
+    });
+  })
+  //not ok!
+  .get("/restaurants", (req, res) => {
+    // var area_query = req.query["‫‪area"];
+    // var categories_query = req.query["category"];
+    // console.log(area_query+"\n"+categories_query);
+    // var result = [];
+    restaurant.model.find({}, (error, foods) => {
       if (error) {
         res.send(error);
       }
       res.send(foods);
     });
   })
-  .get("/restaurants", (req, res) => {
-    var area_query = req.query["‫‪area"];
-    var categories_query = req.query["category"];
-    console.log(area_query+"\n"+categories_query);
-    var result = [];
-    for (i=0;i<this.length(categories_query);i++){
-      restaurant.model.find({ category : categories_query[i] }, (error, foods) => {
-        if (error) {
-          console.log("gandiiii");
-          res.send(error);
-        }
-        console.log("hale cheshat");
-        result.push(foods);
-      });
-    }
-    
-  })
-  .get("/restaurants/:id", (req, res) => {
 
+  //ok!
+  .get("/restaurants/:id", (req, res) => {
+    var param = req.params;
+    console.log(param["id"]);
+    restaurant.model.find({"id":param["id"]}, (error, results) => {
+      if(error){
+        res.send(error);
+      }
+      // res.send(results);
+      result = results[0];
+      res_comments = result["comments"];
+      val = 0;
+      res_comments.forEach(element => {
+        val = val + (element["packaging"] + element["deliveryTime"])/2;
+      });
+      console.log(res_comments.length);
+      val = val/(res_comments.length);
+      //res_comments.length
+      result["averageRate"] = val;
+      result.save("saved!");
+      console.log("val is : ");
+      console.log(val);
+      res.send(result);
+      
+    });
+    
   })
   .get("/restaurants/:id/comments", (req, res) => {
 
@@ -46,6 +74,7 @@ restaurantRouter
   .post("/restaurants/:id/comments", (req, res) => {
 
   })
+  //ok!
   .post("/restaurants", (req, res) => {
     let resObject = new restaurant.model();
     resObject.id = req.body.id;
@@ -54,6 +83,10 @@ restaurantRouter
     resObject.closingTime = req.body.closingTime;
     resObject.address = req.body.address;
     resObject.categories = req.body.categories;
+    resObject.averageRate = req.body.averageRate;
+    resObject.foods = req.body.foods;
+    resObject.comments = req.body.comments;
+    
     resObject.save();
     res.json({
       message: "success"
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 const restaurant = require("./models/restaurant.js");
+const address = require("./models/address.js");
+const category = require("./models/category.js");
+const comment = require("./models/comment.js");
 
 mongoose.connect("mongodb://localhost/web_ta", { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -12,13 +15,22 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("DB connection alive");
   
+  // var new_address = new address.model({id:"1", city:"tehran", area: "jordan", addressLine:"تهران جردن کوچه ۹"});
+  // new_address.save();
+  // var new_category2 = new category.model({id:"kebab", name : "کباب"});
+  // new_category2.save();
+  // var new_category3 = new category.model({id:"salad", name : "سالاد"});
+  // new_category3.save();
+  // var new_category4 = new category.model({id:"fastfood", name : "فست فود"});
+  // new_category4.save();
+
+
+  // var new_restaurant2 = new restaurant.model({id:"shandiz-jordan", name:"جردن", logo: "3.jpg", openingTime: 12, closingTime: 20,
+  // averageRate:3, address: new_address, categories: [new_category2,new_category3,new_category4], foods:[], comments : []});
+  // new_restaurant2.save();
+
   
-  let foodObject = new restaurant.model();
-  // foodObject.id = i;
-  // foodObject.name = food_array[i];
-  foodObject.save();
-  
-  // food.model.remove({ id: "2" }, function(err) {
+  // restaurant.model.remove({}, function(err) {
   //   if (!err) {
   //           console.log("remoded");
   //   }
