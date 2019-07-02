513378af545df19a192cf8955a45e929b0059ab7
mnoroozi <3pid.mollanoroozi@gmail.com>
2019-07-02 19:41:09 +0430

    client folder added and react installed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+const port = 5000;
 const restaurant = require("./models/restaurant.js");
 const address = require("./models/address.js");
 const category = require("./models/category.js");
@@ -14,7 +14,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 db.once("open", function() {
   console.log("DB connection alive");
-  
+
   // var new_address = new address.model({id:"1", city:"tehran", area: "jordan", addressLine:"تهران جردن کوچه ۹"});
   // new_address.save();
   // var new_category2 = new category.model({id:"kebab", name : "کباب"});
@@ -24,12 +24,10 @@ db.once("open", function() {
   // var new_category4 = new category.model({id:"fastfood", name : "فست فود"});
   // new_category4.save();
 
-
   // var new_restaurant2 = new restaurant.model({id:"shandiz-jordan", name:"جردن", logo: "3.jpg", openingTime: 12, closingTime: 20,
   // averageRate:3, address: new_address, categories: [new_category2,new_category3,new_category4], foods:[], comments : []});
   // new_restaurant2.save();
 
-  
   // restaurant.model.remove({}, function(err) {
   //   if (!err) {
   //           console.log("remoded");
@@ -50,4 +48,3 @@ const restaurantRouter = require("./routes/restaurantRouter.js");
 app.use("/api", restaurantRouter);
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
-
